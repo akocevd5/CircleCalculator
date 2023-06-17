b91a15cd9e7a3545997ee586bf70b9d9c86c256a
akocevd5 <119734413+akocevd5@users.noreply.github.com>
2023-06-17 12:31:30 +0200

    Update a menu with options

--- circleCalculator.php
+++ circleCalculator.php
@@ -5,26 +5,54 @@
     <title>Circle Calculator</title>
 </head>
 <body>
-    
-    <h1>Circle Calculator</h1>
-    <form method="POST">
-    <p>Insert radius</p>
-    <input type="number" step="any" name="radius" placeholder="r">
-    <button type="submit" name="submit">Calculate circumference</button>
+
+
+
+<h1>Circle Calculator</h1>
+    <form method="post" action="">
+        <label for="choice">Select an option:</label>
+        <select id="choice" name="choice">
+            <option value="1">Calculate area</option>
+            <option value="2">Calculate circumference</option>
+            <option value="3">Exit</option>
+        </select>
+        <br>
+        <label for="radius">Enter the radius of the circle:</label>
+        <input type="number" id="radius" name="radius" min="0" step="any" required>
+        <br>
+        <button type="submit" name="submit">Submit</button>
     </form>
 
     <?php
-        if($_SERVER["REQUEST_METHOD"]== "POST"){
-            if(isset($_POST["radius"])){
-                $radius = $_POST['radius'];
-                $result = 2 * pi() * $radius;
-                $result = number_format($result,4);
+    function calculate_area($radius) {
+        $result = pi() * pow($radius, 2);
+        $result = number_format($result,4);
+        return $result;
+    }
 
-                echo "<p>C=2πr ≈ $result </p>";
-            }
+    function calculate_circumference($radius) {
+        $result = 2 * pi() * $radius;
+        $result = number_format($result,4);
+        return $result;
+    }
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $choice = $_POST["choice"];
+        $radius = $_POST["radius"];
+
+        if ($choice == 1) {
+            $area = calculate_area($radius);
+            echo "<p>The area of the circle is: $area</p>";
+        } elseif ($choice == 2) {
+            $circumference = calculate_circumference($radius);
+            echo "<p>The circumference of the circle is: $circumference</p>";
+        } elseif ($choice == 3) {
+            echo "<p>Exiting the program.</p>";
         }
-
+    }
     ?>
+
+    
     
 </body>
 </html>
